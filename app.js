fbd711d0e100d5518599a7716214b8cb893709f3
brunolecocq <brunob.lecocq@gmail.com>
2020-04-03 21:15:30 +0200

    Ajout du site

---
+++ app.js
@@ -0,0 +1,22 @@
+(function() {
+    var urlParams = new URLSearchParams(window.location.search);
+
+    if (!urlParams.has("corrige")) {
+        document.querySelectorAll("input").forEach(function (e) {
+            e.value = "";
+            e.checked = false;
+        });
+
+        document.querySelectorAll("textarea").forEach(function (e) {
+            e.innerText = "";
+        });
+    }
+
+    var corrige = document.createElement("div");
+    corrige.innerHTML = `
+<a href="?corrige" class="button primary">Voir les réponses</a>
+<a href="?" class="button primary">Masquer les réponses</a>
+<a href="index.html" class="button primary">Retour à l'accueil</a>`;
+    corrige.classList.add("footer");
+    document.body.append(corrige);
+})();
